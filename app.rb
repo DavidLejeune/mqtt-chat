dcd70e59b5abbd1302acb13f79fc9f85e1effb36
David Lejeune <Lejeune.David@outlook.com>
2016-11-08 15:05:36 +0100

    BASIC SEXY INTERFACE

--- app.rb
+++ app.rb
@@ -1,15 +1,57 @@
 require 'rubygems'
 require 'mqtt'
 
-# Publish example
-MQTT::Client.connect('10.177.33.144') do |c|
-  c.publish('test', 'message')
+
+
+
+
+def show_intro
+  system "clear"
+
+
+  #Startup screen
+  puts '      ____              __        '
+  puts '     / __ \\   ____ _   / /      ___ '
+  puts '    / / / /  / __ `/  / /      / _ \\'
+  puts '   / /_/ /  / /_/ /  / /___   /  __/'
+  puts '  /_____/   \\__,_/  /_____/   \\___/ '
+  puts ''
+  puts '     +-+-+-+-+ +-+-+-+-+-+-+-+-+'
+  puts '     |R|u|b|y| |t|e|r|m|i|n|a|l|'
+  puts '     +-+-+-+-+ +-+-+-+-+-+-+-+-+'
+  puts ''
+  puts ' >> Author : David Lejeune'
+  puts ''
+  puts ' ###########################################'
+  puts ' #            MQTT Chat Machine            #'
+  puts ' ###########################################'
+  puts ''
 end
 
-# Subscribe example
-MQTT::Client.connect('10.177.33.144') do |c|
-  # If you pass a block to the get method, then it will loop
-  c.get('test') do |topic,message|
-    puts "#{topic}: #{message}"
+
+
+
+    def publish_message
+      # Publish example
+      MQTT::Client.connect('10.177.33.144') do |c|
+        c.publish('test', 'message')
+      end
   end
-end
+
+    def subscribe_message
+        # Subscribe example
+        MQTT::Client.connect('10.177.33.144') do |c|
+          # If you pass a block to the get method, then it will loop
+          c.get('test') do |topic,message|
+            puts "#{topic}: #{message}"
+          end
+        end
+  end
+
+
+
+
+
+
+  show_intro
+  Puts 'Enter your name'
