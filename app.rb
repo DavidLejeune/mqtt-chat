77cd3cc2e76c22a0e2f603986c3a56d22dd2e359
David Lejeune <Lejeune.David@outlook.com>
2016-11-08 14:57:16 +0100

    add ip address of server (sille)

--- app.rb
+++ app.rb
@@ -2,12 +2,12 @@ require 'rubygems'
 require 'mqtt'
 
 # Publish example
-MQTT::Client.connect('test.mosquitto.org') do |c|
+MQTT::Client.connect('10.177.33.144') do |c|
   c.publish('test', 'message')
 end
 
 # Subscribe example
-MQTT::Client.connect('test.mosquitto.org') do |c|
+MQTT::Client.connect('10.177.33.144') do |c|
   # If you pass a block to the get method, then it will loop
   c.get('test') do |topic,message|
     puts "#{topic}: #{message}"
